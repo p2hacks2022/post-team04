703068363204db7a02a261c5f9854cf9c2fbf8fa
harueight <public8610@gmail.com>
2022-12-17 23:02:26 +0900

    add: 割る窓の数を数えてくれるプログラム、すべての窓を割ったら「ゲームクリア！」と表示するようにした。

---
+++ Assets/Easy Weapons/Scripts/WindowManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WindowManager : MonoBehaviour
+{
+    private GameObject[] windowBox;
+
+    void Update()
+    {
+        windowBox = GameObject.FindGameObjectsWithTag("glass");
+        
+        Debug.Log("窓の数：" + windowBox.Length);
+
+        if (windowBox.Length == 0)
+        {
+            Debug.Log("クリア！");
+        }
+    }
+}
