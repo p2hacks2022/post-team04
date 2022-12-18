64d49b6478ee171c82e504d645198acd77f1603b
harueight <public8610@gmail.com>
2022-12-18 11:37:41 +0900

    wip: 敵に当たったら、最初の画面に戻る

---
+++ Assets/Scripts/atarihantei.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;//スクリプトの先頭に書く
+
+public class atarihantei : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision c)
+    { //何かと当たったら1回呼ばれる
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);//シーン変更
+    }
+
+}
