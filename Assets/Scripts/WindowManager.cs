99f5e266e4f74b7cc0b8c4368d05fbdb068ade9e
harueight <public8610@gmail.com>
2022-12-18 10:46:11 +0900

    add: クリア画面遷移、窓の待った枚数のカウント

---
+++ Assets/Scripts/WindowManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class WindowManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject clearUI;
+
+    private GameObject[] windowBox;
+
+    void Update()
+    {
+        windowBox = GameObject.FindGameObjectsWithTag("glass");
+
+        print("窓の数：" + windowBox.Length);
+
+        if (windowBox.Length == 0)
+        {
+            clearUI.SetActive(true);
+            Debug.Log("クリア！");
+        }
+    }
+}
